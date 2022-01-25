fa3edc931b6db20cf8e3ec1f5c9db08f823bcd2c
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-01-25 14:51:54 -0600

    Curso Ionic | Buttons

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -23,7 +23,18 @@ export class InicioPage implements OnInit {
       icon:'alert-circle-outline',
       name:'Alert',
       redirectTo:'/alert'
-    }
+    },
+    {
+      icon:'beaker-outline',
+      name:'Avatar',
+      redirectTo:'/avatar'
+    },
+    {
+      icon:'radio-button-off-outline',
+      name:'Buttons',
+      redirectTo:'/button'
+    },
+
   ];
 
   constructor() { }
