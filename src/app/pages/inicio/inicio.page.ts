c8efc669e6b8a56cfdcb06e600cbdd75c3c76a9c
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-01-26 16:29:48 -0600

    Curso Ionic | Menu

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Componente {
-  icon:string;
-  name: string;
-  redirectTo:string;
-}
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from '../../interfaces/interfaces';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-inicio',
@@ -13,83 +11,17 @@ interface Componente {
 })
 export class InicioPage implements OnInit {
 
-  componentes:Componente[] = [
-    {
-      icon:'american-football-outline',
-      name:'Action sheet',
-      redirectTo:'/action-sheet'
-    },
-    {
-      icon:'alert-circle-outline',
-      name:'Alert',
-      redirectTo:'/alert'
-    },
-    {
-      icon:'beaker-outline',
-      name:'Avatar',
-      redirectTo:'/avatar'
-    },
-    {
-      icon:'radio-button-off-outline',
-      name:'Buttons',
-      redirectTo:'/button'
-    },
-    {
-      icon:'card-outline',
-      name:'Cards',
-      redirectTo:'/card'
-    },
-    {
-      icon:'checkmark-circle-outline',
-      name:'Checks',
-      redirectTo:'/check'
-    },
-    {
-      icon:'calendar-outline',
-      name:'DateTime',
-      redirectTo:'/date-time'
-    },
-    {
-      icon:'car-outline',
-      name:'Fab',
-      redirectTo:'/fab'
-    },
-    {
-      icon:'grid-outline',
-      name:'Grid',
-      redirectTo:'/grid'
-    },
-    {
-      icon:'infinite-outline',
-      name:'Infinite',
-      redirectTo:'/infinite'
-    },
-    {
-      icon:'hammer-outline',
-      name:'Input',
-      redirectTo:'/input'
-    },
-    {
-      icon:'list-outline',
-      name:'List - Sliding',
-      redirectTo:'/list'
-    },
-    {
-      icon:'reorder-three-outline',
-      name:'List - Reorder',
-      redirectTo:'/list-reorder'
-    },
-    {
-      icon:'refresh-circle-outline',
-      name:'Loading',
-      redirectTo:'/loading'
-    },
+  //componentes: Componente[] = [];
+  componentes : Observable<Componente[]>
 
-  ];
-
-  constructor() { }
+  constructor(private menuCtrl:MenuController, private dataService: DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
+  }
+
+  mostrarMenu(){
+    this.menuCtrl.open('first')
   }
 
 }
